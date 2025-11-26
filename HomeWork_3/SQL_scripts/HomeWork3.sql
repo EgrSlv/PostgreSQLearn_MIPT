292ca5af8d1fe8de8bd7928038bd87e1afdfda28
MalkinTe <malkinte@student.school-21.ru>
2025-11-26 21:20:32 +0300

    [HW3] задания 1-8 выполнены!

--- HomeWork_3/SQL_scripts/HomeWork3.sql
+++ HomeWork_3/SQL_scripts/HomeWork3.sql
@@ -4,12 +4,10 @@
       отсортировав результат по убыванию количества.
 */
 select
-	job_industry_category,
+	coalesce(job_industry_category, 'n\a') as job_industry_category,
 	COUNT(customer_id) as num_customers
 from
 	customer
-where
-	job_industry_category is not null
 group by
 	job_industry_category
 order by
@@ -23,24 +21,32 @@ order by
       месяцу и сфере деятельности.
 */
 with approved_order_revenues as(
-	select customer_id, o.order_id, order_date, revenues.revenue 
-	from orders o
+	select
+		customer_id,
+		o.order_id,
+		order_date,
+		revenues.revenue
+	from
+		orders o
 	left join (
-		select order_id, quantity * item_list_price_at_sale revenue
-		from order_items) as revenues on o.order_id = revenues.order_id 
-	where order_status = 'Approved'
+		select
+			order_id,
+			quantity * item_list_price_at_sale revenue
+		from
+			order_items) revenues on
+		o.order_id = revenues.order_id
+	where
+		order_status = 'Approved'
 )
 select
-	c.job_industry_category,
+	coalesce(c.job_industry_category, 'n\a') as job_industry_category,
 	extract(year from aor.order_date) as year,
-	extract(month from aor.order_date) as month,	
-	TRUNC(sum(aor.revenue)::numeric, 2) revenue
+	extract(month from aor.order_date) as month,
+	TRUNC(sum(aor.revenue)::numeric, 2) as revenue
 from
 	approved_order_revenues aor
 left join customer c on
 	c.customer_id = aor.customer_id
-where
-	c.job_industry_category is not null
 group by
 	year,
 	month,
@@ -75,32 +81,121 @@ with unique_approved_orders_IT as (
 		and o.order_status = 'Approved'
 )
 select
-	distinct brand,
-	count(order_id) over (partition by brand) num_orders
+	brand,
+	count(order_id) num_orders
 from
 	product_cor pc
 left join unique_approved_orders_IT uao on
 	uao.product_id = pc.product_id
 	and uao.online_order = true
-order by num_orders desc
+group by
+	brand
+order by
+	num_orders desc
 ;
 
 /* Query 4
  * 
  * 4. Найти по всем клиентам: сумму всех заказов (общего дохода), максимум, минимум
       и количество заказов, а также среднюю сумму заказа по каждому клиенту.
-      Отсортироватьрезультат по убыванию суммы всех заказов и количества заказов. 
+      Отсортировать результат по убыванию суммы всех заказов и количества заказов. 
       Выполнить двумя способами: используя только GROUP BY 
-      и используя только оконные функции. Сравнитьрезультат.
+      и используя только оконные функции. Сравнить результат.
  */
-
+with revenue_items as (
+	select
+		c.customer_id,
+		o.order_id,
+		trunc(oi.revenue::numeric, 2) as revenue
+	from
+		customer c
+	left join (
+		select
+			customer_id,
+			order_id
+		from
+			orders
+	) o on c.customer_id = o.customer_id
+	left join (
+		select
+			order_id,
+			(quantity * item_list_price_at_sale) as revenue
+		from
+			order_items
+	) oi on oi.order_id = o.order_id
+),
+cte_with_group_by as (
+	select
+		customer_id,
+		sum(revenue) sum_revenue,
+		max(revenue) max_revenue,
+		min(revenue) min_revenue,
+		count(order_id) num_orders,
+		trunc(avg(revenue)::numeric, 2) avg_revenue
+	from
+		revenue_items
+	group by
+		customer_id
+),
+cte_with_over as (
+	select
+		--distinct
+		customer_id,
+		sum(revenue) over(partition by customer_id) sum_revenue,
+		max(revenue) over(partition by customer_id) max_revenue,
+		min(revenue) over(partition by customer_id) min_revenue,
+		count(order_id) over(partition by customer_id) num_orders,
+		trunc(avg(revenue) over(partition by customer_id)::numeric, 2) avg_revenue
+	from
+		revenue_items
+)
+select
+	*
+from
+--	cte_with_group_by
+	cte_with_over
+order by
+	sum_revenue desc,
+	num_orders desc
+;
 
 /* Query 5
  * 
  * 5. Найти имена и фамилии клиентов с топ-3 минимальной
       и топ-3 максимальной суммой транзакций за весь период
-      (учесть клиентов, у которых нет заказов, приняв их суммутранзакций за 0).
+      (учесть клиентов, у которых нет заказов, приняв их сумму транзакций за 0).
  */
+with transaction_amount_per_customer as (
+	select
+		o.customer_id,
+		trunc(coalesce(sum(oi.quantity * oi.item_list_price_at_sale), 0)::numeric, 2) as amount
+	from
+		orders as o
+	join order_items as oi on oi.order_id = o.order_id
+	group by
+		o.customer_id
+),
+transactions_rank as (
+	select
+		customer_id,
+		amount,
+		rank() over(order by amount asc) as top3_min_amount,
+		rank() over(order by amount desc) as top3_max_amount
+	from
+		transaction_amount_per_customer
+)
+select
+	c.first_name,
+	c.last_name,
+	coalesce(tr.amount, 0) as amount,
+	coalesce(top3_min_amount, 0) as top3_min_amount,
+	coalesce(top3_max_amount, 0) as top3_max_amount
+from
+	transactions_rank tr
+right join (	select customer_id, first_name, coalesce(last_name, 'n\a') as last_name
+			from customer) c on c.customer_id = tr.customer_id
+where tr.top3_min_amount <=3 or tr.top3_max_amount <=3
+;
 
 /* Query 6
  * 
@@ -108,6 +203,30 @@ order by num_orders desc
       с помощью оконных функций. Если у клиента меньше двух транзакций,
       он не должен попасть в результат.
  */
+with only_second_customer_orders as (
+	select
+		o.order_id,
+		c.customer_id,
+		lead(o.order_id) over(partition by c.customer_id) as second_order_id,
+		row_number() over(partition by c.customer_id) as rn
+	from
+		customer c
+	left join orders as o
+			using(customer_id)
+)
+select
+	o.*
+from
+	orders o
+join only_second_customer_orders as osco on
+	osco.order_id = o.order_id
+where
+	second_order_id is not null
+	and rn = 1
+	-- не забываем, что сделали сдвиг через lead()
+order by
+	o.order_id
+;
 
 /* Query 7
  * 
@@ -116,10 +235,71 @@ order by num_orders desc
       между двумя последовательными заказами. Исключить клиентов,
       у которых только один или меньше заказов.
  */
+with calc_intervals as (
+	select
+		o.order_id,
+		c.first_name,
+		c.last_name,
+		c.job_title,
+		count(o.order_id) over(partition by o.customer_id) as num_orders,
+		lead(order_date) over(partition by o.customer_id order by order_date) - order_date as days
+	from
+		customer c
+	join orders o on
+		c.customer_id = o.customer_id
+)
+select
+	first_name,
+	coalesce(last_name, 'n\a') as last_name,
+	coalesce(job_title, 'n\a') as job_title,
+	max(days) as max_interval_days
+	--	,max(num_orders) as num_orders
+from
+	calc_intervals
+where
+	num_orders > 1
+group by
+	first_name,
+	last_name,
+	job_title
+order by
+	max(days) desc 
+;
 
 /* Query 8
  * 
  * 8. Найти топ-5 клиентов (по общему доходу) в каждом сегменте благосостояния
       (wealth_segment). Вывести имя, фамилию, сегмент и общий доход.
-      Если в сегменте менее 5клиентов, вывести всех.
+      Если в сегменте менее 5 клиентов, вывести всех.
  */
+with transaction_amount_per_customer as (
+	select
+		o.customer_id,
+		trunc(coalesce(sum(oi.quantity * oi.item_list_price_at_sale), 0)::numeric, 2) as amount
+	from
+		orders as o
+	join order_items as oi on
+		oi.order_id = o.order_id
+	group by
+		o.customer_id
+)
+select
+	*
+from
+	(
+	select
+		c.first_name,
+		coalesce(c.last_name, 'n\a') as last_name,
+		c.wealth_segment,
+		coalesce(t.amount, 0) as amount,
+		row_number() over(partition by wealth_segment order by amount desc) as rn
+	from
+		customer c
+	left join transaction_amount_per_customer as t on
+		t.customer_id = c.customer_id
+	where
+		amount > 0
+) as ranks
+where
+	rn <= 5
+;
